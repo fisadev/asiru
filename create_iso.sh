56f8fc869f275a79b9994b948983f3c906d0fa21
Juan Pedro Fisanotti <fisadev@gmail.com>
2011-08-17 11:28:43 -0300

    Typo on generated iso path

--- create_iso.sh
+++ create_iso.sh
@@ -72,6 +72,6 @@ sudo rm md5sum.txt
 find -type f -print0 | sudo xargs -0 md5sum | grep -v isolinux/boot.cat | sudo tee md5sum.txt
 
 echo "Compiling image..."
-sudo mkisofs -D -r -V "$IMAGE_NAME" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o ./asiru.iso .
+sudo mkisofs -D -r -V "$IMAGE_NAME" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o ../asiru.iso .
 cd ..
 echo "Asiru image ready to use!"
