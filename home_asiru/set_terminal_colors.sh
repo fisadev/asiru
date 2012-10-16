5431ba0f9b3b6c0954442cf34c90ded273ceed53
Juan Pedro Fisanotti <fisadev@gmail.com>
2012-10-16 14:13:51 -0300

    Added autostart commands to set gnome terminal colors

---
+++ home_asiru/set_terminal_colors.sh
@@ -0,0 +1,4 @@
+# gnome terminal colors
+gconftool-2 --set "/apps/gnome-terminal/profiles/Default/background_color" --type string "#111111111111"
+gconftool-2 --set "/apps/gnome-terminal/profiles/Default/foreground_color" --type string "#FFFFFFFFFFFF"
+gconftool-2 --set "/apps/gnome-terminal/profiles/Default/use_theme_colors" --type bool false
