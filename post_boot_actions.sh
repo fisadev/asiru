2d610cc33a9935699da62dee14e9259e7b007a2f
Juan Pedro Fisanotti <fisadev@gmail.com>
2012-10-16 00:13:24 -0300

    Modify gnome-terminal colors after boot

--- post_boot_actions.sh
+++ post_boot_actions.sh
@@ -2,3 +2,8 @@
 sudo rsync -a -r /opt/asiru/home_asiru/ /home/ubuntu/
 sudo chown ubuntu /home/ubuntu -R
 sudo chgrp ubuntu /home/ubuntu -R
+
+# gnome terminal colors
+gconftool-2 --set "/apps/gnome-terminal/profiles/Default/background_color" --type string "#111111111111"
+gconftool-2 --set "/apps/gnome-terminal/profiles/Default/foreground_color" --type string "#FFFFFFFFFFFF"
+gconftool-2 --set "/apps/gnome-terminal/profiles/Default/use_theme_colors" --type bool false
